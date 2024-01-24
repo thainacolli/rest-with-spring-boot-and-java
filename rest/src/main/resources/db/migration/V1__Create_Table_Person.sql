060b2242016e4487d21306390ade19bdcf429f35
Thaina Colli <thaina.alves@first-tech.net>
2024-01-24 15:27:43 -0300

    utilização de migrations com flyway

---
+++ rest/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `person` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
---
+++ rest/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` VALUES
+    (1,'Maria','Silva','São Paulo','Feminino'),
+    (2,'José','Amaral','Minas Gerais','Masculino'),
+    (3,'Alice', 'Louveira','Pernambuco','Feminino'),
+    (4,'Isaac','Alves','São Paulo','Masculino'),
+    (5,'Eduarda','Lopes','Mato Grosso','Feminino'),
+    (6,'Rogerio','Amaral','São Paulo','Masculino');
