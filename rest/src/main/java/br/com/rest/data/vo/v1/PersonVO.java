2835a9eb4adfd3fe0a9e5d9ea332b8d87db20215
Thaina Colli <thaina.alves@first-tech.net>
2024-01-24 16:05:56 -0300

    serialização JSON customizada

--- rest/src/main/java/br/com/rest/data/vo/v1/PersonVO.java
+++ rest/src/main/java/br/com/rest/data/vo/v1/PersonVO.java
@@ -1,5 +1,8 @@
 package br.com.rest.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -7,14 +10,21 @@ import java.util.Objects;
 
 
 
+@JsonPropertyOrder({"id", "address", "firstName", "lastName", "gender"})
 public class PersonVO implements Serializable {
 
 
 
     private long id;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
     private String address;
+
+    @JsonIgnore
     private String gender;
 
     public PersonVO() {
